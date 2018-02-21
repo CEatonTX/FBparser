741f7a1426bf7b37169db7177fb476cffb191999
Chris Eaton <chriseatontx@gmail.com>
2018-02-21 09:18:16 -0600

    First real code, this has a regex that appears to be working and it counts the number of UID's (total, not unique) in the file.

--- fbparser.pl
+++ fbparser.pl
@@ -1,5 +1,51 @@
-use strict;
+#!/usr/bin/perl\
+# FBswparser Version 0.01
+# Chris Eaton
+# This is intended for Law Enforcement use
+# Contact ceaton@wacotx.gov
 
-print "Test 911\n";
+use v5.22.1;                           #enable features
+use strict;                            #always do this
+use warnings;                          #always do this
 
-die;
+my $filename= 'unified_messages/index.html';
+my $fh= undef; my $NumberOfUIDs= undef; my $found= undef;my $countlines= 0;
+my $countuids= 0; my $countlineuids= 0;my $numfinds= 0; my @uids= undef;
+my $uid= undef; my $x= undef; my $y- undef; my @uidsinline= undef;
+my $count = undef;
+
+open ($fh, '<', $filename)
+  or die "cannot open '$filename error: $!";
+
+say "Processing \"$filename...\"", "\n";
+
+while ( $_ = readline($fh) ) {        #Shorthand for: while ($_ = readline($fh)) {
+  $countlines++;      #Keep track of how many lines we read in this file
+  $countlineuids = 1;                 #Reset this during each loop.
+
+  @uidsinline = /id=(100\d{12})/g;
+  if (@uidsinline) {      #if a UID found then do this
+    foreach $x (@uidsinline) { #Count how many UIDs there are
+      $countuids++;      #Keep track of total UIDs
+      $countlineuids++; #count the number of UIDs in this line only
+    }
+
+    if ($countlineuids > 1) {say "I found $countlineuids UIDs in line $countlines";}
+      else {say "I found one UID in line $countlines";
+    }
+
+    say "Now processing each UID in line $countlines:\n";        #this is for debuging
+    say "size of array \@uidsinline:", scalar(@uidsinline);
+
+    $count = 0;
+    foreach my $x (@uidsinline) {
+      $count++;
+      say "working on UID $x";
+      say "checking to see if UID $x is in the array \@uids";
+      if ($x) {say "x=", $x;}
+      say "\$count=", $count;
+    }
+  }
+}
+say "Found $countuids UIDs in $countlines lines in \"$filename\"";
+die "Could not find any UID's " unless($fh); #code dies with nice error
